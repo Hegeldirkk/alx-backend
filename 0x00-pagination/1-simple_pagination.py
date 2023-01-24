c0ce58bd8956590cf4038b51e5dbff8cd415e07d
Hegeldirkk <hegeldirkk@gmail.com>
2023-01-24 05:11:13 +0000

    script git writting - ok

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -34,7 +34,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """verify that both arguments are integers greater than 0"""
+        """verify that both arguments are integers greater than 0 """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
