619e5dce5cd7cd9d4fab632c97f5dc786d3caf4b
Hegeldirkk <hegeldirkk@gmail.com>
2023-01-30 06:30:05 +0000

    script git writting - rnu lfu fifo

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" LIFO Caching
+"""
+
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """inherits  caching system"""
+    def __init__(self):
+        super().__init__()
+        self.__key = []
+
+    def put(self, key, item):
+        """put cahce"""
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__key:
+            discard = self.__key.pop()
+            del self.cache_data[discard]
+            print('DISCARD: {}'.format(discard))
+        if key and item:
+            self.__key.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get value"""
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
